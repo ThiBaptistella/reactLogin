58aee6a636994bb0d02e571ee2a31699dc84dbab
thiago <thiago_baptistella1@hotmail.com>
2017-11-12 22:24:00 +0100

    First commit

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+/*
+    ./client/components/App.jsx
+*/
+//import react
+import React from 'react';
+//export this componet to render in  index.js
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>);
+  }
+}
---
+++ client/index.js
@@ -0,0 +1,9 @@
+/*
+    ./client/index.js
+*/
+//import react dependencies and the componet App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+//render component on index.html class div class='root'
+ReactDOM.render(<App />, document.getElementById('root'));
